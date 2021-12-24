7244c5563301920a2b7c2fae45091ed8d6eb812a
Uzlopak <aras.abbasi@googlemail.com>
2021-12-24 15:08:34 +0100

    Test2

--- src/Entry.ts
+++ src/Entry.ts
@@ -146,15 +146,7 @@ export class Entry<U extends ITransaction = ITransaction, J extends IJournal = I
 
 console.log(insertedIds);
 
-      if (insertedIds.length === this.transactions.length && !insertedIds[0]) {
-        // Mongo returns `undefined` as the insertedIds when forceServerObjectId=true. Let's re-read it.
-        const txs = await transactionModel.collection
-          .find({ _journal: this.transactions[0]._journal }, { projection: { _id: 1 }, session: options.session })
-          .toArray();
-        insertedIds = txs.map((tx) => tx._id as Types.ObjectId);
-      }
-
-      (this.journal._transactions as Types.ObjectId[]).push(...insertedIds);
+      this.journal._transactions = insertedIds as Types.ObjectId[];
       await this.journal.save(options);
 
       if (options.writelockAccounts && options.session) {
